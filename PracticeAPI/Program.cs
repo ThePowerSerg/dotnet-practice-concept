6117885ab68cc4a0eab6499fe23054f8ef1a3896
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-04 11:37:08 -0400

    Add service comment

--- PracticeAPI/Program.cs
+++ PracticeAPI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// StoreContext service
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
