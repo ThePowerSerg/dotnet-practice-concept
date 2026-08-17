994dda6804b44b3148251809025dea59bf2d07ff
ThePowerSerg <sferreir@bu.edu>
2026-08-17 09:36:32 -0400

    Add inline data test

--- MessagingTests/UserProfileServiceTests.cs
+++ MessagingTests/UserProfileServiceTests.cs
@@ -33,5 +33,23 @@ namespace MessagingTests
             Assert.Equal(1, result.Id);
             Assert.Equal("Ada", result.UserName);
         }
+
+        [Theory]
+        [InlineData("sergiof810", 2)]
+        public async Task GetUserProfileByIdInline(string expectedValue, int id)
+        {
+            // Arrange: Define the repository call the mock should recognize and what sholud be 
+            // returned when the service requests the UserProfile 1
+            repository.Setup(repo => repo.GetUserProfileByIdAsync(id))
+                      .ReturnsAsync(new UserProfile { Id = 2, UserName = "sergiof810" });
+
+            // Act
+            var result = await service.GetUserProfileByIdAsync(2);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Id);
+            Assert.Equal(expectedValue, result.UserName);
+        }
     }
 }
